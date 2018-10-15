f7c553f5d5e8a60e0697f3850d69ee4d3a10c075
DerpDays <34582078+DerpDays@users.noreply.github.com>
2018-10-15 22:30:23 +0100

    Update __init__.py

--- tobio/__init__.py
+++ tobio/__init__.py
@@ -1,1 +1,16 @@
-Some serious beep-boop to come
+import discord
+client = discord.Client()
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Activity(name="Hi", type=0))
+    print("Bot now connected!")
+    print("------------------------------")
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('ping'):
+        await message.channel.send('pong!')
+client.run('TOKEN')
+##THIS IS REWRITE THOUGH
